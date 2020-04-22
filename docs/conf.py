9554a9ab760475f465c7a429db19368267b2e701
giadaspasiano <64136773+giadaspasiano@users.noreply.github.com>
2020-04-22 12:38:18 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ source_parsers = {
 }
 project = 'Tesina'
 release = '0.0.1'
-author = u'Giada Spasiano'
+author = u'Giada Spasiano - III F'
 
 show_authors = True
 # Grouping the document tree into LaTeX files. List of tuples
